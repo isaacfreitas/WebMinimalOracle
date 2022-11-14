acd18cb5511bc5ee508f676d782dc3f2552c1113
isaacfreitas <isaacfrancafreitas@outlook.com>
2022-11-14 00:56:42 -0300

    teste

---
+++ WebMinimalOracle/Config/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMinimalOracle.Models;
+
+namespace WebMinimalOracle.Config
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            //usar só em teste
+            // Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
--- WebMinimalOracle/Program.cs
+++ WebMinimalOracle/Program.cs
@@ -1,6 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using WebMinimalOracle.Config;
+using WebMinimalOracle.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var stringConexao = "User Id=SYSTEM;Password=1234;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST =localhost)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=XE)))";
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseOracle(stringConexao));
+
 var app = builder.Build();
+app.UseSwagger();
 
-app.MapGet("/", () => "Hello World!");
 
+app.MapPost("AdicionaProduto", async (Produto produto, Contexto contexto) =>
+{
+    contexto.Produto.Add(produto);
+    await contexto.SaveChangesAsync();
+});
+
+
+app.MapPost("ExcluirProduto/{id}", async (int id, Contexto contexto) =>
+{
+    var produto = await contexto.Produto.FirstOrDefaultAsync(p => p.Id == id);
+    if (produto != null)
+    {
+        contexto.Produto.Remove(produto);
+        await contexto.SaveChangesAsync();
+    }
+});
+
+app.MapGet("ListarProdutos", async (Contexto contexto) =>
+{
+    return await contexto.Produto.ToListAsync();
+});
+
+app.MapGet("ObterProduto/{id}", async (int id, Contexto contexto) =>
+{
+    return await contexto.Produto.FirstOrDefaultAsync(p => p.Id == id);
+});
+
+
+
+app.UseSwaggerUI();
 app.Run();
